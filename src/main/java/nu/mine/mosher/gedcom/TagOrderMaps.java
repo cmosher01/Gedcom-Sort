26f2a1b1ab747890b692d5cdcf57bb0db79f7985
Christopher A. Mosher <cmosher01@gmail.com>
2017-09-02 19:56:21 -0400

    Typo in HEAD (DESI -> DEST); rearrange HEAD sort order

--- src/main/java/nu/mine/mosher/gedcom/TagOrderMaps.java
+++ src/main/java/nu/mine/mosher/gedcom/TagOrderMaps.java
@@ -11,28 +11,39 @@ public final class TagOrderMaps {
 
     public static final Map<GedcomTag, Integer> mapTopLevelOrder = Collections.unmodifiableMap(new HashMap<GedcomTag, Integer>() {{
         int i = 0;
-        put(GedcomTag.HEAD, i++); put(GedcomTag.SUBN, i++); put(GedcomTag.SUBM, i++);
-        put(GedcomTag.INDI, i++); put(GedcomTag.FAM, i++);
-        put(GedcomTag.REPO, i++); put(GedcomTag.SOUR, i++); put(GedcomTag.NOTE, i++); put(GedcomTag.OBJE, i++);
+        put(GedcomTag.HEAD, i++);
+        put(GedcomTag.SUBN, i++);
+        put(GedcomTag.SUBM, i++);
+
+        put(GedcomTag.INDI, i++);
+        put(GedcomTag.FAM, i++);
+
+        put(GedcomTag.REPO, i++);
+        put(GedcomTag.SOUR, i++);
+
+        put(GedcomTag.OBJE, i++);
+        put(GedcomTag.NOTE, i++);
+
         put(GedcomTag.TRLR, i++);
     }});
 
     public static final Map<GedcomTag, Integer> mapHeadOrder = Collections.unmodifiableMap(new HashMap<GedcomTag, Integer>() {{
         int i = 0;
         put(GedcomTag.CHAR, i++);
-        put(GedcomTag.LANG, i++);
-        put(GedcomTag.PLAC, i++);
-        put(GedcomTag.COPR, i++);
+        put(GedcomTag.GEDC, i++);
 
         put(GedcomTag.SOUR, i++);
-        put(GedcomTag.DESI, i++);
-        put(GedcomTag.DATE, i++);
-        put(GedcomTag.SUBM, i++);
-        put(GedcomTag.SUBN, i++);
-        put(GedcomTag.FILE, i++);
-        put(GedcomTag.NOTE, i++);
+        put(GedcomTag.DEST, i++);
+        put(GedcomTag.PLAC, i++);
 
-        put(GedcomTag.GEDC, i++);
+        put(GedcomTag.COPR, i++);
+        put(GedcomTag.SUBN, i++);
+        put(GedcomTag.SUBM, i++);
+
+        put(GedcomTag.FILE, i++);
+        put(GedcomTag.DATE, i++);
+        put(GedcomTag.LANG, i++);
+        put(GedcomTag.NOTE, i++);
     }});
 
     public static final Map<GedcomTag, Integer> mapEventOrder = Collections.unmodifiableMap(new HashMap<GedcomTag, Integer>() {{
